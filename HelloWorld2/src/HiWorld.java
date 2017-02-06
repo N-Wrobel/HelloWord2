8686ff9ecb0a9e83e038fee850d8f084ddc85bd4
Natasha <Natasha.Wrobel@maine.edu>
2017-02-06 16:35:30 -0500

    Added display for date

--- HelloWorld2/src/HiWorld.java
+++ HelloWorld2/src/HiWorld.java
@@ -1,6 +1,8 @@
+import java.util.Calendar;
 
 public class HiWorld {
 	public static void main(String args[]) {
 		System.out.println("Hello world!");
+		System.out.println("Today's date is " + Calendar.getInstance().getTime());
 	}
 }
